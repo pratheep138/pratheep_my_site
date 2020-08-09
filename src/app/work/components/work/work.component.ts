8873f628747809e1765fc92479953dcd8aff0da7
Sandeep Sand <sandeep.sand66@gmail.com>
2020-08-09 16:32:58 +0530

    feedback changes

--- src/app/work/components/work/work.component.ts
+++ src/app/work/components/work/work.component.ts
@@ -11,11 +11,21 @@ export class WorkComponent implements OnInit {
   public work1: string;
   public work2: string;
 
+  public company1: string;
+  public company2: string;
+
+  public techIcons: string;
+
   constructor(private readonly router: Router) { }
 
   ngOnInit(): void {
     this.work1 = 'assets/images/professional.png';
     this.work2 = 'assets/images/achievements.png';
+
+    this.company1 = 'assets/images/HCL.png';
+    this.company2 = 'assets/images/TCS.png';
+
+    this.techIcons = 'assets/images/';
   }
 
   public navigateTo(url: string) {
